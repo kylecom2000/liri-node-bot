be840bdb7c657d2aadd2836a82051da4c01da2f2
FightingSprrow <kbauertx@gmail.com>
2018-11-23 13:43:31 -0600

    working concert-this command

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('keys.js is loaded');
 
-exports.spotify = {
+exports.spotifyKeys = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,18 +1,19 @@
-// ODESSA or ALLEGRA
+// NODE program
+
 
 // Requiring the .env file up first.
 require('dotenv').config();
 // Variables enabling the specified modules available to liri.js.
-const axios = require('axios');
-const Spotify = require('node-spotify-api');
-const moment = require('moment');
-const chalk = require('chalk');
+const axios = require("axios");
+const Spotify = require("node-spotify-api");
+const moment = require("moment");
+const chalk = require("chalk");
 
 
 // Making the keys.js file available to be referenced.
 const keys = require("./keys.js");
-// Code required to import the `keys.js` file and store it in a variable. 
-const spotify = new Spotify(keys.spotify);
+// Import the `keys.js` and store it in a variable. 
+const spotify = new Spotify(keys.spotifyKeys);
 
 // Assigning arguments to variables. 
 liriDoThis = process.argv[2];
@@ -24,52 +25,79 @@ You have selected: {green ${liriDoThis}}
 To search for: {green ${liriLookThis}}
 `);
 
+// Run liribot function with these things.
 liriBot(liriDoThis, liriLookThis);
 
 
-// 9. Make it so liri.js can take in one of the following commands:
+// Function performing each choice using a switch.
 function liriBot (liriDo, liriLook){
     
 
     switch (liriDo) {
-
-//--------------------------------concert-this-----------------------------------------
+//--------------------------------concert----------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+//----------------------------------------this-----------------------------------------------------------------
         case "concert-this":
-            console.log("Concerts from ", liriLook, ":");
-            var queryUrl = "https://rest.bandsintown.com/artists/" + liriLook + "/events?app_id=codingbootcamp";
-            axios.get(queryUrl).then(function(res){
-                res.data.forEach(function(concert){
-                    var concertDate = concert.datetime.split("T");
-                    var momentDate = moment(concertDate.shift.format("MM/DD/YYYY"));
-                    console.log(chalk`
-Venue: {red ${concert.venue}}
-Location: {blue ${concert.venue.city},  ${concert.venue.region},  ${concert.venue.country}}
-Date: {green ${momentDate}}
-`);
-                });
-               
-                
+            // Display what user looked up:
+            console.log("Concerts from ",liriLook,":");
 
+            // Set the URL for Bands in Town with search term variable.
+            var queryUrl = "https://rest.bandsintown.com/artists/" + liriLook + "/events?app_id=codingbootcamp";
+
+            // Using axios to get the query.
+            axios.get(queryUrl).then(function(res, err){
+                // For each response object, perform this function
+                res.data.forEach(function(concert){
+                    // Converting date to my format.
+                    var concertDate = concert.datetime.split("T");
+                    var momentDate = moment(concertDate[0]).format("DDMMMYYYY");
+                    // Prints the information
+                    console.log(chalk`
+Venue: {red ${concert.venue.name}}
+Locaiton: {redBright ${concert.venue.city}},{magenta ${concert.venue.region}},{white ${concert.venue.country}}
+Date: {green ${momentDate}}
+                    `);
+                }); // End of forEach.
 
             }).catch(function(err){
                 console.log("There seems to be a problem.");
-            });
-            break;
+            }); 
+
+            break; // End of concert-this.
 
 
-//--------------------------------spotify-this-song-------------------------------------
+//--------------------------------spotify-----------------------------------------------------------------------
+//----------------------------------------this------------------------------------------------------------------
+//---------------------------------------------song-------------------------------------------------------------
         case "spotify-this-song":
             console.log("spotify-this-song was checked.")
+            spotify.search({
+                type: "track", query: liriLook, limit: 5
+                }).then(function(res){
+                    // console.log(res);
+                    res.tracks.items.forEach(function(songs){
+                        console.log(chalk`
+Artist: {red ${songs.artists[0].name}}
+Song Title: {green ${songs.name}}
+Album: {blue ${songs.album.name}}
+Spotify Link: {cyan {bgBlue ${songs.external_urls.spotify}}}
+                        `);
+                    });
+                });
             break;
 
 
-//-----------------------------------movie-this-----------------------------------------
+//-----------------------------------movie----------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------
+//-----------------------------------------this-----------------------------------------------------------------
         case "movie-this":
             console.log("movie-this was checked.")
             break;
 
 
-//-----------------------------------movie-this-----------------------------------------
+//-----------------------------------do-------------------------------------------------------------------------
+//--------------------------------------what-it-----------------------------------------------------------------
+//-----------------------------------------------says-----------------------------------------------------------
         case "do-what-it-says":
             console.log("do-what-it-says was checked.")
             break;
@@ -78,5 +106,8 @@ Date: {green ${momentDate}}
             const randomResponse = ["I'm sorry, that doesn't go there.", "Put down the drink and type with two hands!", "Please try that again. I'm a robot, not a linguist."];
             console.log(randomResponse[(Math.floor(Math.random() * Math.floor(randomResponse.length)))]);
             break;
-    }
-}
+
+    
+    }   // -----End of Switch------
+
+}   // ------End of liriBot Function-----
